76bcaf2941e29703ac320da14c9d50e0d0f93748
Kaluã Bentes <kaluanbentes@gmail.com>
2025-03-29 21:17:16 -0300

    Feat: Organize and add new songs

--- src/app/data.ts
+++ src/app/data.ts
@@ -6,27 +6,27 @@ export const albums = [
   {
     id: 1,
     name: "Immortalized",
-    cover: "/mvp-cover.png",
+    cover: "/albums/immortalized/cover.png",
     year: "2025",
     songs: [
       {
         id: 1,
-        src: "/ajuricaba-inner-core.mp3",
+        src: "/albums/immortalized/ajuricaba-inner-core.mp3",
         name: "Inner Core",
       },
       {
         id: 2,
-        src: "/ajuricaba-leaving-orbit.mp3",
+        src: "/albums/immortalized/ajuricaba-leaving-orbit.mp3",
         name: "Leaving Orbit",
       },
       {
         id: 3,
-        src: "/ajuricaba-apocalypse.mp3",
+        src: "/albums/immortalized/ajuricaba-apocalypse.mp3",
         name: "Apocalypse",
       },
       {
         id: 4,
-        src: "/ajuricaba-murderer.mp3",
+        src: "/albums/immortalized/ajuricaba-murderer.mp3",
         name: "Murderer",
       },
     ],
